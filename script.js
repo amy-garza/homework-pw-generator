0aa21488aa5f0d946633d4e0ed651a87b1d2267b
Amy Garza <amygarza.tx@gmail.com>
2020-10-12 14:44:50 -0500

    add function; change var keyword to let; set variables

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+// Set variables
+
+let charLength = 12;
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+// Add function 
+
+function generatePassword () {
 
 }
