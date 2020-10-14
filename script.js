339d196621a619bf3c1ffba893e1cb7a6fb647ad
Amy Garza <amygarza.tx@gmail.com>
2020-10-14 16:53:27 -0500

    add remaining true conditional statements

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 // Declare and assign variables
-
 let passwordLength = 8;
 let characterList = [];
 
 // Declare and initialize string / array(s) to store password characters and types
-
-let specialCharacters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ " ; 
 let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let numeric = ['0','1','2','3','4','5','6','7','8','9'];
+let specialCharacters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ " ; 
+let numerics = ['0','1','2','3','4','5','6','7','8','9'];
 
 // Assignment Code
-
 let generateBtn = document.querySelector("#generate");
 
 // Add event listener to generate button
-
 generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
@@ -29,12 +24,10 @@ function writePassword() {
 }
 
 // Define function
-
 function generatePassword() {
 }
 
 // Create prompt box to generatePassword following prompts
-
 function getPrompts(){
   passwordLength = parseInt(prompt("Enter the number of characters you'd like for your password. (Must be 8 to 128 characters)"));
 
@@ -45,8 +38,17 @@ function getPrompts(){
       return false;
   }
 
-  if(confirm("Include lower case letters in your password?")) {
+  if(confirm("Include lowercase letters in your password?")) {
     characterList = characterList.concat(lowerCase);
   }
-
+  if(confirm("Include UPPERCASE letters in your password?")) {
+    characterList = characterList.concat(upperCase);
+  }
+  if(confirm("Include special characters in your password? (e.g., !, ?, #)")) {
+    characterList = characterList.concat(specialCharacters);
+  }
+  if(confirm("Include numbers in your password? (e.g., !, ?, #)")) {
+    characterList = characterList.concat(numerics);
+  }
+  return true;
 }
