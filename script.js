d13b0bf26c5553b555df9f923e8df2bf58ae30f7
Amy Garza <amygarza.tx@gmail.com>
2020-10-14 16:37:50 -0500

    create conditional statement for lowercase letters

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let numeric = ['0','1','2','3','4','5','6','7','8','9'];
 
-
-
 // Assignment Code
 
 let generateBtn = document.querySelector("#generate");
@@ -43,7 +41,12 @@ function getPrompts(){
 // 
 
   if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128 {
-    
+      alert("Error: Please enter a numeral from 8 to 128.");
+      return false;
+  }
+
+  if(confirm("Include lower case letters in your password?")) {
+    characterList = characterList.concat(lowerCase);
   }
 
 }
