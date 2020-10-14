66eabe03010ea6ba0310f7cf7ac0322a9350a770
Amy Garza <amygarza.tx@gmail.com>
2020-10-14 11:18:59 -0500

    add values to array

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Declare and assign variables
 
 let passwordLength = 12;
-let selectionArray = [];
+let characterList = [];
 
-// Declare array(s) to store password characters and types
+// Declare and initialize string / array(s) to store password characters and types
 
-let specialCharacters = [];
-let lowerCase = [];
-let upperCase = [];
-let numeric = [];
-
+let specialCharacters = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ " ; 
+let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let numeric = ['0','1','2','3','4','5','6','7','8','9'];
 
 
 
