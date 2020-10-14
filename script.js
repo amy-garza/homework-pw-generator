c89a366bba7f0078d6fcf915c9421d6a16abe388
Amy Garza <amygarza.tx@gmail.com>
2020-10-14 17:18:30 -0500

    include additional comments and correct syntax errors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let characterList = [];
 // Declare and initialize string / array(s) to store password characters and types
 let lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-let specialCharacters = ['!','#','$','%','&','*','?','@']; 
+let specialCharacters = ['!','#','$','%','&','*','?','@','.','_']; 
 let numerics = ['0','1','2','3','4','5','6','7','8','9'];
 
 // Assignment Code
@@ -29,25 +29,26 @@ function generatePassword() {
 
 // Create prompt box to generatePassword following prompts
 function getPrompts(){
-  passwordLength = parseInt(prompt("Enter the number of characters you'd like for your password. (Must be 8 to 128 characters)"));
+  passwordLength = parseInt(prompt("Enter the number of characters for your password. (Must be 8 to 128 characters)"));
 
-// 
+// Stop running for Not A Number entry or not within criteria length range
 
-  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128 {
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       alert("Error: Please enter a numeral from 8 to 128.");
       return false;
   }
 
+// Creates prompt for true or valid "OK"
   if(confirm("Include lowercase letters in your password?")) {
     characterList = characterList.concat(lowerCase);
   }
   if(confirm("Include UPPERCASE letters in your password?")) {
     characterList = characterList.concat(upperCase);
   }
-  if(confirm("Include special characters in your password? (e.g., !, ?, #)")) {
+  if(confirm("Include special characters in your password?")) {
     characterList = characterList.concat(specialCharacters);
   }
-  if(confirm("Include numbers in your password? (e.g., !, ?, #)")) {
+  if(confirm("Include numbers in your password?")) {
     characterList = characterList.concat(numerics);
   }
   return true;
